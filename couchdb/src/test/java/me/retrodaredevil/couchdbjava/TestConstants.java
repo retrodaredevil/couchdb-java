4b62af043cf5475ddc98a8a3c7da6499e917dbcb
Joshua Shannon <retrodaredevil@gmail.com>
2021-07-11 20:48:16 -0500

    Started to add integration test with @Tag annotation

---
+++ couchdb/src/test/java/me/retrodaredevil/couchdbjava/TestConstants.java
@@ -0,0 +1,7 @@
+package me.retrodaredevil.couchdbjava;
+
+public final class TestConstants {
+	private TestConstants() { throw new UnsupportedOperationException(); }
+
+	public static final String INTEGRATION_TEST = "integration";
+}
---
+++ couchdb/src/test/java/me/retrodaredevil/couchdbjava/integration/DatabaseCreateTest.java
@@ -0,0 +1,15 @@
+package me.retrodaredevil.couchdbjava.integration;
+
+import me.retrodaredevil.couchdbjava.TestConstants;
+import org.junit.jupiter.api.Tag;
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.assertTrue;
+
+@Tag(TestConstants.INTEGRATION_TEST)
+public class DatabaseCreateTest {
+	@Test
+	void test() {
+		assertTrue(false);
+	}
+}
